7d12da07f4ffee055824fd832921a687ab429b56
brepowell <85724630+brepowell@users.noreply.github.com>
2022-12-13 19:37:33 -0800

    added more switches and text fields

--- app/src/main/java/com/example/passive_cooling/ui/ScreenMainMenu.kt
+++ app/src/main/java/com/example/passive_cooling/ui/ScreenMainMenu.kt
@@ -1,31 +1,94 @@
 package com.example.passive_cooling.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Switch
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenMainMenu(
     //onLocationSharingChanged: (Boolean) -> Unit,
     //onUpdatePasswordClicked: () -> Unit,
     //onHelpButtonClicked: () -> Unit
 ) {
+    val directionOptions = listOf("North", "East", "South", "West")
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        /** ------- PROMPT ----------------- */
+        Text(text = "Welcome to Keep It Cool! \n" +
+                "Please input your information")
+        
+        Spacer(modifier = Modifier.size(16.dp))
 
-    Column (horizontalAlignment = Alignment.CenterHorizontally) {
+        /** ------- TEXT FIELDS ----------------- */
+        var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+            mutableStateOf(TextFieldValue("example", TextRange(0, 7)))
+        }
 
-        var checked by remember { mutableStateOf(true) }
-        Switch(
-            modifier = Modifier.semantics { contentDescription = "Demo" },
-            checked = checked,
-            onCheckedChange = { checked = it })
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("City") },
+            placeholder = { Text("Bothell") }
+        )
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("State") },
+            placeholder = { Text("WA") }
+        )
+
+        CreateSwitch("Allergies today")
+        CreateSwitch("Noise outside today")
+
+        /** ------- TOGGLE SWITCHES FOR DIRECTION ----------------- */
+        var i = 0
+        repeat(directionOptions.size) {
+            CreateSwitch(directionOptions[i])
+            i++
+        }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateSwitch(text: String) {
+    var checked by remember { mutableStateOf(true) }
+
+    Row(
+        Modifier.selectableGroup(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    )
+
+    {
+        Column {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(start = 36.dp, end = 36.dp)
+            )
+        }
+
+        Column(
+        ) {
+            Switch(
+                modifier = Modifier.semantics { contentDescription = "Demo" },
+                checked = checked,
+                onCheckedChange = { checked = it })
+        }
+    }
+}
 
 //+++++++++++++++++++++PREVIEW +++++++++++++++++
 @Preview(showBackground = true)
