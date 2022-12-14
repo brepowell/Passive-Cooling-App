8486332bef65442a8ffe246cd74e702ead67ed12
brepowell <85724630+brepowell@users.noreply.github.com>
2022-12-13 18:34:06 -0800

    added switch

--- app/src/main/java/com/example/passive_cooling/ui/ScreenMainMenu.kt
+++ app/src/main/java/com/example/passive_cooling/ui/ScreenMainMenu.kt
@@ -1,8 +1,12 @@
 package com.example.passive_cooling.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -14,6 +18,11 @@ fun ScreenMainMenu(
 
     Column (horizontalAlignment = Alignment.CenterHorizontally) {
 
+        var checked by remember { mutableStateOf(true) }
+        Switch(
+            modifier = Modifier.semantics { contentDescription = "Demo" },
+            checked = checked,
+            onCheckedChange = { checked = it })
     }
 }
 
